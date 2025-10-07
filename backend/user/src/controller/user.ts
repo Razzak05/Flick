8cd5c4de926b83b567a91452cef37d9502f6742a
Razzak05 <razzakchoudhary9619@gmail.com>
2025-10-07 19:53:36 +0530

    small change for production

--- backend/user/src/controller/user.ts
+++ backend/user/src/controller/user.ts
@@ -141,8 +141,8 @@ export const verifyOtp = async (req: Request, res: Response) => {
 
     res.cookie("accessToken", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
